a273a6e9e7d70105d55c49901f1ddf188e69e04e
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-02-20 12:13:08 +0000

    Update test file following tutorial. Add header divs to index and new views. Wrap header div in index view and form command in new view in matching Turbo frames so that clicking on "New quote" link puts form on index page.

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,38 +2,49 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:one)
+    @quote = quotes(:first)
   end
 
-  test "visiting the index" do
-    visit quotes_url
-    assert_selector "h1", text: "Quotes"
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector 'h1', text: @quote.name
   end
 
-  test "should create quote" do
-    visit quotes_url
+  test "Creating a new quote" do
+    visit quotes_path
+    assert_selector 'h1', text: "Quotes"
+  
     click_on "New quote"
+    fill_in "Name", with: "Capybara quote"
 
-    click_on "Create Quote"
+    assert_selector "h1", text: "Quotes"
+    click_on "Create quote"
 
-    assert_text "Quote was successfully created"
-    click_on "Back"
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
   end
 
-  test "should update Quote" do
-    visit quote_url(@quote)
-    click_on "Edit this quote", match: :first
+  test "Updating a quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+    
+    click_on "Edit", match: :first
+    fill_in "Name", with: "Updated quote"
 
-    click_on "Update Quote"
+    assert_selector "h1", text: "Quotes"
+    click_on "Update quote"
 
-    assert_text "Quote was successfully updated"
-    click_on "Back"
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
   end
 
-  test "should destroy Quote" do
-    visit quote_url(@quote)
-    click_on "Destroy this quote", match: :first
-
-    assert_text "Quote was successfully destroyed"
+  test "Destroying a quote" do
+    visit quotes_path
+    assert_text @quote.name
+    
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
   end
 end
